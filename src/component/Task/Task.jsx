969bf40e16d8feb63d95cd20ca9015792e0daf03
Anil Adhikari <adhikarianil719@gmail.com>
2024-02-12 23:00:05 +0545

    UI updated

--- src/component/Task/Task.jsx
+++ src/component/Task/Task.jsx
@@ -5,7 +5,7 @@ import Completed from './Completed'
 
 function Task() {
   return (
-    <div className="flex flex-row justify-around p-3">
+    <div className="flex flex-row justify-around p-4 mt-1 box-border w-full border-4  rounded-lg">
         <Unstarted/>
         <Pending/>
         <Completed/>
